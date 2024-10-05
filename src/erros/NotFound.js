1799b3d01ce68841249bf0731558e4b79a0ae198
FelipeSouzaG <154008572+FelipeSouzaG@users.noreply.github.com>
2024-10-05 01:15:36 -0300

    Delete src directory

--- src/erros/NotFound.js
+++
@@ -1,9 +0,0 @@
-import ErrorBase from "./ErrorBase.js";
-
-class NotFound extends ErrorBase {
-  constructor(msg = "Página não encontrada."){
-    super(msg, 404);
-  }
-}
-
-export default NotFound;
