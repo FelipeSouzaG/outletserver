f432dd81e05bc84fb632e0d0a7d21b82e3ad6da2
FelipeSouzaG <154008572+FelipeSouzaG@users.noreply.github.com>
2024-10-05 01:10:14 -0300

    Delete src/routes directory

--- src/routes/publicRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import ProductController from "../controllers/ProductController.js";
-
-const router = express.Router();
-
-router
-  .get("/products", ProductController.listPublicProducts);
-
-export default router;
